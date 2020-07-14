b6375beabfdd82bcfca1008633b687efb6fe6227
BoBoooooo <17746714@qq.com>
2020-07-14 17:23:12 +0800

    feat(MVue): 新增MVue.js

---
+++ src/MVue.js
@@ -0,0 +1,12 @@
+/*
+ * @file: MVue 源码简单实现
+ * @copyright: NanJing Anshare Tech .Com
+ * @author: BoBo
+ * @Date: 2020年07月14 15:24:01
+ */ 
+
+ class MVuew{
+   constructor(options){
+      
+   }
+ }
