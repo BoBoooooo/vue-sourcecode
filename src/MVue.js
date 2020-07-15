1a6d0f027e335351a9140fb5bf4a14088030d7f5
BoBoooooo <17746714@qq.com>
2020-07-15 09:42:37 +0800

    feat(MVue): 几处代码跳转

--- src/MVue.js
+++ src/MVue.js
@@ -5,6 +5,7 @@
  * @Date: 2020年07月14 15:24:01
  */
 
+ // 指令解析器
 class Compile {
   constructor(el, vm) {
     this.el = this.isElementNode(el)
@@ -18,19 +19,22 @@ class Compile {
 
     // 编译模板
 
-    this.compile(this.$el);
+    this.compile(fragment);
     // 2.追加子元素到根元素
     this.el.appendChild(fragment);
   }
   compile(fragment) {
     // 1.获取子节点
     const childNodes = fragment.childNodes;
+    console.log(childNodes);
     [...childNodes].forEach((child) => {
+      console.log(child);
       if (this.isElementNode(child)) {
         // 是元素结点
         // 编译元素结点
         console.log("元素节点", child);
       } else {
+        // 文本结点
         console.log("文本节点", child);
       }
       if (Array.isArray(child.childNodes)) {
@@ -38,21 +42,23 @@ class Compile {
       }
     });
   }
+  // dom结点转为文档碎片对象
   node2Fragment(el) {
     const f = document.createDocumentFragment();
     let firstChild;
-    while ((firstChild = el.firstChild)) {
+    while (firstChild = el.firstChild) {
       f.appendChild(firstChild);
     }
     return f;
   }
+  // 判断是否为dom结点
   isElementNode(node) {
     return node.nodeType === 1;
   }
 }
 
 
-class MVuew {
+class MVue {
   constructor(options) {
     this.$el = options.el;
     this.$data = options.data;
@@ -61,7 +67,6 @@ class MVuew {
       // 1.实现一个数据观察者
 
       // 2. 实现一个指令解析器
-
       new Compile(this.$el, this);
     }
   }
