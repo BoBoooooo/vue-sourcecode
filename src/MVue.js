91ed18ba10e433a7de9e69cdfa0a2c1bd4b992ec
BoBoooooo <17746714@qq.com>
2020-07-15 17:23:30 +0800

    feat(event): 初步实现compile指令解析,支持on bind model html text

--- src/MVue.js
+++ src/MVue.js
@@ -5,7 +5,58 @@
  * @Date: 2020年07月14 15:24:01
  */
 
- // 指令解析器
+// 指令解析对象
+const compileUtil = {
+  getVal(expr, vm) {
+    return expr.split(".").reduce((data, currentVal) => {
+      return data[currentVal];
+    }, vm.$data);
+  },
+  // 解析v-text指令,以及模板{{ }} 语法
+  text(node, expr, vm) {
+    let value;
+    // 解析所有 {{ }} 到对应值
+    if (expr.includes("{{")) {
+      value = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+        return this.getVal(args[1], vm);
+      });
+    } else {
+      // 解析v-text到对应值
+      value = this.getVal(expr, vm);
+    }
+    this.updater.textUpdater(node, value);
+  },
+  html(node, expr, vm) {
+    const value = this.getVal(expr, vm);
+    this.updater.htmlUpdater(node, value);
+  },
+  model(node, expr, vm) {
+    const value = this.getVal(expr, vm);
+    this.updater.modelUpdater(node, value);
+  },
+  on(node, expr, vm, eventName) {
+    const fn =
+      vm.$options.methods && vm.$options.methods[expr].bind(vm);
+    node.addEventListener(eventName, fn, false);
+  },
+  bind(node,expr,vm,eventName){
+    const value = this.getVal(expr, vm);
+    node.setAttribute(eventName,value)
+  },
+  updater: {
+    textUpdater(node, value) {
+      node.textContent = value;
+    },
+    htmlUpdater(node, value) {
+      node.innerHTML = value;
+    },
+    modelUpdater(node, value) {
+      node.value = value;
+    },
+  },
+};
+
+// 指令解析器
 class Compile {
   constructor(el, vm) {
     this.el = this.isElementNode(el)
@@ -15,7 +66,6 @@ class Compile {
     // 1.获取文档碎片对象 放入内存中减少页面回流和重绘
 
     const fragment = this.node2Fragment(this.el);
-    console.log(fragment);
 
     // 编译模板
 
@@ -26,27 +76,66 @@ class Compile {
   compile(fragment) {
     // 1.获取子节点
     const childNodes = fragment.childNodes;
-    console.log(childNodes);
     [...childNodes].forEach((child) => {
-      console.log(child);
       if (this.isElementNode(child)) {
-        // 是元素结点
         // 编译元素结点
-        console.log("元素节点", child);
+        this.compileElement(child);
       } else {
-        // 文本结点
-        console.log("文本节点", child);
+        // 编译文本结点
+        this.compileText(child);
       }
-      if (Array.isArray(child.childNodes)) {
+      if (Array.isArray([...child.childNodes])) {
         this.compile(child);
       }
     });
   }
+  // 编译元素结点
+  compileElement(node) {
+    const { attributes } = node;
+    [...attributes].forEach((attr) => {
+      const { name, value } = attr;
+      // 判断当前元素结点属性是否含有vue指令 "v-"
+      if (this.isDirective(name)) {
+        // v-text , v-html , v-on ,v-model等
+        const [, dirctive] = name.split("-"); // text , html , on , model
+        const [dirName, eventName] = dirctive.split(":"); // 指令名,事件名
+        // 指令解析器,更新数据 数据驱动视图
+        compileUtil[dirName](node, value, this.vm, eventName);
+        // 删除有指令标签上的属性
+        node.removeAttribute("v-" + dirctive);
+      }
+      // 判断是否使用了缩写 例如@click="xxx"
+      else if (this.isEventName(name)) {
+        const [, eventName] = name.split("@");
+        compileUtil["on"](node, value, this.vm, eventName);
+      }
+    });
+  }
+
+  // 判断属性是否为vue指令
+  isDirective(attrName) {
+    return attrName.startsWith("v-");
+  }
+  // 判断是否为@缩写
+  isEventName(attrName) {
+    return attrName.startsWith("@");
+  }
+  // 编译文本结点
+  compileText(node) {
+    const content = node.textContent;
+    // 解析模板语法的情况
+    // {{}}   /\{\{
+    if (/\{\{(.+?)\}\}/.test(content)) {
+      return compileUtil.text(node, content, this.vm);
+    }
+    return content;
+  }
+
   // dom结点转为文档碎片对象
   node2Fragment(el) {
     const f = document.createDocumentFragment();
     let firstChild;
-    while (firstChild = el.firstChild) {
+    while ((firstChild = el.firstChild)) {
       f.appendChild(firstChild);
     }
     return f;
@@ -56,7 +145,6 @@ class Compile {
     return node.nodeType === 1;
   }
 }
-
 
 class MVue {
   constructor(options) {
