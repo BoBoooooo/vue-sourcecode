1267f1501a93318e54e2955a388c8538a799081d
BoBoooooo <17746714@qq.com>
2020-07-16 16:24:20 +0800

    feat(webpack): 新增webpack init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+/*
+ * @file: 
+ * @copyright: NanJing Anshare Tech .Com
+ * @author: BoBo
+ * @Date: 2020年07月16 15:14:21
+ */ 
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/*
+ * @file: 
+ * @copyright: NanJing Anshare Tech .Com
+ * @author: BoBo
+ * @Date: 2020年07月16 15:21:39
+ */ 
+const path = require('path');
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry : path.resolve(__dirname,'src/js/MVue.js'),
+    output :{
+        filename : 'src/js/[name]-[hash].js',
+        path : path.resolve(__dirname,'dist')
+    },
+    devServer : {
+        disableHostCheck : true
+    },
+    module : {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+    plugins : [
+        new htmlWebpackPlugin({
+            filename : 'public/index.html',
+            template : 'public/index.html',
+            minify : {
+                removeComments : true, //去注释
+                collapseWhitespace : true //去空格
+            }
+        })
+    ]
+}
