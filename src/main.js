bc140deb77f31742416acd74de2b228e33783947
BoBoooooo <17746714@qq.com>
2021-02-18 15:05:39 +0800

    perf: some change

--- src/main.js
+++ src/main.js
@@ -17,11 +17,14 @@ window.vm = new MVue({
     },
     msg: "Vue源码简单实现",
     model:'input框model',
-    imageSrc:'https://pic.downk.cc/item/5e78290a5c56091129507571.png'
+    imageSrc:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.yipic.cn%2Fthumb%2F2aeb8272%2Fa9ecb08b%2Ffd950107%2Ff695968b%2Fbig_2aeb8272a9ecb08bfd950107f695968b.png&refer=http%3A%2F%2Fimg.yipic.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616223841&t=efa0a0088e5b0a568fe7d79b7d64665f'
   },
   methods:{
     handleClick(index){
       this.person.name = '沈飞飞';
+    },
+    handleClick2(index){
+      this.person.name = '赵四';
     }
   }
 });
